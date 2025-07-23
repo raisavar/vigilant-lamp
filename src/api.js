782a32015d6b40eae6dd653f78df68993b6af802
raisavar <raisavar@users.noreply.github.com>
2025-07-23 04:12:53 +0300

    build: add logging

--- src/api.js
+++ src/api.js
@@ -77,3 +77,30 @@ class API {
 
 // Update 41
 module.exports = API;
+
+
+// API module for LampServer
+
+class API {
+    constructor() {
+        this.routes = {};
+    }
+    
+    registerRoute(path, handler) {
+        this.routes[path] = handler;
+    }
+    
+    handleRequest(method, path, data = {}) {
+        if (this.routes[path]) {
+            return this.routes[path](data);
+        }
+        return { error: 'Not found' };
+    }
+    
+    getRoutes() {
+        return Object.keys(this.routes);
+    }
+}
+
+// Update 48
+module.exports = API;
