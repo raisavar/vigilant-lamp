23e212aa27f4c438dc9825e97d876a6992bc299b
raisavar <raisavar@users.noreply.github.com>
2025-12-23 05:45:14 +0200

    test: code refactoring

--- src/api.js
+++ src/api.js
@@ -131,3 +131,30 @@ class API {
 
 // Update 49
 module.exports = API;
+
+
+// API module for LampServer
+
+class API {
+    constructor() {
+        this.routes = {};
+    }
+    
+    registerRoute(path, handler) {
+        this.routes[path] = handler;
+    }
+    
+    handleRequest(method, path, data = {}) {
+        if (this.routes[path]) {
+            return this.routes[path](data);
+        }
+        return { error: 'Not found' };
+    }
+    
+    getRoutes() {
+        return Object.keys(this.routes);
+    }
+}
+
+// Update 50
+module.exports = API;
