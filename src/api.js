9307a6d1fcaa43c05de6ad9c185bf8c6d9f8d3d5
raisavar <raisavar@users.noreply.github.com>
2026-01-21 11:52:37 +0200

    build(core): fix broken tests

--- src/api.js
+++ src/api.js
@@ -50,3 +50,30 @@ class API {
 
 // Update 10
 module.exports = API;
+
+
+// API module for LampServer
+
+class API {
+    constructor() {
+        this.routes = {};
+    }
+    
+    registerRoute(path, handler) {
+        this.routes[path] = handler;
+    }
+    
+    handleRequest(method, path, data = {}) {
+        if (this.routes[path]) {
+            return this.routes[path](data);
+        }
+        return { error: 'Not found' };
+    }
+    
+    getRoutes() {
+        return Object.keys(this.routes);
+    }
+}
+
+// Update 41
+module.exports = API;
